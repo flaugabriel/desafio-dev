23f4b06cfee26e6d471d458555f800226a243b29
gabriel.flauzino@vindi.com.br <flaugabriel@gmail.com>
2023-05-16 14:27:32 -0400

    first commit by configs and install more libs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "jest": true
+    },
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+      "sourceType": "module",
+      "ecmaVersion": 2020
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      "react/react-in-jsx-scope": "off",
+      "indent": ["warn", 4],
+      "react/jsx-indent": ["warn", 4, { "checkAttributes": true}],
+      "react/destructuring-assignment": "off",
+      "no-nested-ternary": "warn",
+      "react/prop-types": "warn"
+    }
+  }
